6766eb19a893e3e825e45446661f815d238c15dd
Pranay_Khairnar <khairnarpranay89@gmail.com>
2026-02-05 17:31:11 +0530

    JAVA ADDING

---
+++ PQR.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class PQR {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of p:");
+        int p=sc.nextInt(); 
+        System.out.println("Enter the value of q:");
+        int q=sc.nextInt();
+        int r=p+q;
+        float z=(float)p/q;
+        System.out.println("Sum of p and q is: " + r);
+        System.out.println("Division of p and q is: " + z);       
+    }
+}
